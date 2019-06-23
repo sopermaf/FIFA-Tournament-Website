1e241b6eff78a5fe1e22f4524d55aac543cd7c51
Ferdia Soper Mac Cafraidh <ferdia@omko.co>
2019-06-23 12:10:45 +0100

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-btn flat href="">
+        <span class="mr-2"> Home</span>
+      </v-btn>
+      <v-btn flat href="">
+        <span class="mr-2"> League Table</span>
+      </v-btn>
+      <v-btn flat href="">
+        <span class="mr-2"> Descriptions</span>
+      </v-btn>
+
+      <v-spacer></v-spacer>
+      
+      <v-toolbar-title class="headline text-uppercase">
+        <span>FIFA</span>
+        <span class="font-weight-light">Tournament</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      
+    </v-content>
+
+    <v-footer height="auto" color="primary lighten-1">
+      <v-layout justify-center row wrap>
+        <v-flex primary lighten-2 py-3 text-xs-center white--text md12>
+          &copy;2019 — <strong>Fifa Tournament</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ frontend/vue.config.js
@@ -0,0 +1,52 @@
+const BundleTracker = require("webpack-bundle-tracker");
+
+const pages = {
+    'fifa_app': {
+        entry: './src/main.js',
+        chunks: ['chunk-vendors']
+    }
+}
+
+module.exports = {
+    baseUrl: "http://127.0.0.1:8080/",
+    outputDir: './dist/',
+    pages: pages,
+
+    chainWebpack: config => {
+        
+        config.optimization
+            .splitChunks({
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "chunk-vendors",
+                        chunks: "all",
+                        priority: 1
+                    },
+                },
+            });
+        
+        // avoid creating unused pages as Django backend
+        Object.keys(pages).forEach(page => {
+            config.plugins.delete(`html-${page}`);
+            config.plugins.delete(`preload-${page}`);
+            config.plugins.delete(`prefetch-${page}`);
+        })
+
+        config
+            .plugin('BundleTracker')
+            .use(BundleTracker, [{filename: '../frontend/webpack-stats.json'}])
+
+        config.resolve.alias
+            .set('__STATIC__', 'static')
+
+        config.devServer
+            .public('http://0.0.0.0:8080')
+            .host('0.0.0.0')
+            .port(8080)
+            .hotOnly(true)
+            .watchOptions({poll: 1000})
+            .https(false)
+            .headers({"Access-Control-Allow-Origin": ["\*"]})
+            }
+        };
