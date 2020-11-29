6c1d0b644d7e920cbe6d7f835f60e361dc0e45b3
ferdia <ferdia@omko.co>
2020-11-28 22:22:13 -0600

    site url fixed to redirect to fifa app for base url

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fifa/', include("fifa.urls")),
+    path('', include("fifa.urls")),
 ]
