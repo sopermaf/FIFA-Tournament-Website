4960ecfa7b77ce0e394d9b20ee6277261d76fdc8
ferdia <ferdia@omko.co>
2020-12-01 22:47:42 -0600

    added games app and removed fifa app from settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'players.apps.PlayersConfig',
     'games.apps.GamesConfig',
-    'fifa.apps.FifaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
