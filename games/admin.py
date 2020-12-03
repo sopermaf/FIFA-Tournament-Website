fdfde8f33e788352264b9886f20622596f0a74d3
ferdia <ferdia@omko.co>
2020-12-03 14:57:08 -0600

    admin view added for games

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Team
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('time', 'played', 'tv', 'modified')
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stars', 'player_name', 'game')
+
+    def player_name(self, team):
+        return team.player.user.username
+
+    def game_name(self, team):
+        if self.team.game:
+            return str(self.team.game)
+        return "---"
